e5665f93ed62f6e912a7f6c2a0b7cbe149e10552
scottraiford <88979496+scottraiford@users.noreply.github.com>
2021-09-10 03:36:23 -0400

    Support specified Portainer admin password, add X

--- system.sh
+++ system.sh
@@ -5,10 +5,20 @@ pacman -Syu
 # Install podman with Docker compatibility
 echo "Install Podman"
 pacman -S --noconfirm podman podman-docker
+systemctl enable --now podman.socket
 
 # Configure podman to support docker.io registry
 patch -N /etc/containers/registries.conf data/configs/registries.conf.patch
 
+# Create container storage
+mkdir -p /srv/containers
+mkdir /srv/containers/portainer_data
+
+# Hash admin password
+hashed=$(podman run --rm httpd:2.4-alpine htpasswd -nbB admin $PORTAINER_ADMIN_PASSWORD | cut -d ":" -f 2)
+
 # Install Portainer
+podman run -d -p 9000:9000 --privileged --name=portainer --restart=always -v /run/podman/podman.sock:/var/run/docker.sock:Z -v /srv/containers/portainer_data:/data portainer/portainer-ce --admin-password=$hashed
 
 # Install host X server
+pacman -S --noconfirm xorg-server
