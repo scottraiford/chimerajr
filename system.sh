d200c9d9db651794ab7851340d5fed70a6b39553
scottraiford <88979496+scottraiford@users.noreply.github.com>
2021-09-10 04:19:17 -0400

    Added X with autostart

--- system.sh
+++ system.sh
@@ -25,4 +25,27 @@ hashed=$(podman run --rm httpd:2.4-alpine htpasswd -nbB admin $PORTAINER_ADMIN_P
 podman run -d -p 9000:9000 --privileged --name=portainer --restart=always -v /run/podman/podman.sock:/var/run/docker.sock:Z -v /srv/containers/portainer_data:/data portainer/portainer-ce --admin-password=$hashed
 
 # Install host X server
-pacman -S --noconfirm xorg-server
+pacman -S --noconfirm xorg-server xf86-video-fbdev
+
+# Create X unit file
+cat <<EOF > /usr/lib/systemd/system/X.service
+[Unit]
+Description=X window server
+After=network.target network-online.target nss-lookup.target
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=3
+AmbientCapabilities=CAP_NET_RAW
+ExecStart=/usr/bin/X -nocursor
+TimeoutStopSec=10
+KillMode=mixed
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable X on startup
+systemctl daemon-reload
+systemctl enable --now X.service
