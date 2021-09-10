1fb60e51f7b7521ef592ace64ec4c31ffda3685a
scottraiford <88979496+scottraiford@users.noreply.github.com>
2021-09-10 02:45:30 -0400

    Update system.sh

--- system.sh
+++ system.sh
@@ -2,6 +2,13 @@
 echo "Updating base system"
 pacman -Syu
 
-# Install podman
+# Install podman with Docker compatibility
 echo "Install Podman"
-pacman -S --noconfirm podman
+pacman -S --noconfirm podman podman-docker
+
+# Configure podman to support docker.io registry
+patch /etc/containers/registries.conf data/configs/registries.conf.patch
+
+# Install Portainer
+
+# Install host X server
