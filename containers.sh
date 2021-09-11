3f12e7d700e101dcf0bc301461f6759bab2c31a6
scottraiford <88979496+scottraiford@users.noreply.github.com>
2021-09-11 15:57:07 -0400

    Daemonize containers

--- containers.sh
+++ containers.sh
@@ -2,6 +2,7 @@
 
 # Build container images
 podman build containers/berry/ -t berry
+podman build containers/controller/ -t controller
 
 # Make storage locations for containers that need them
 mkdir /srv/containers/portainer_data
@@ -10,13 +11,18 @@ mkdir /srv/containers/portainer_data
 podman run -d -p 9000:9000 --privileged --name=portainer --restart=always -v /run/podman/podman.sock:/var/run/docker.sock:Z -v /srv/containers/portainer_data:/data portainer/portainer-ce --admin-password=$hashed
 
 # Run Berry
-podman run --name berry --restart always -v /home/chimerajr:/home/chimerajr -v /tmp/.X11-unix/:/tmp/.X11-unix/ -e DISPLAY berry
+podman run -d --name berry --restart always -v /home/chimerajr:/home/chimerajr -v /tmp/.X11-unix/:/tmp/.X11-unix/ -e DISPLAY berry
+
+# Run Controller
+podman run -d --name berry --restart always -v /home/chimerajr:/home/chimerajr -v /tmp/.X11-unix/:/tmp/.X11-unix/ -e DISPLAY controller
 
 # Create systemd unit files to rebuild containers on startup
 pushd /usr/lib/systemd/system/
 podman generate systemd -n --new -f portainer
 podman generate systemd -n --new -f berry
+podman generate systemd -n --new -f controller
 systemctl daemon-reload
 systemctl enable --now container-portainer.service
 systemctl enable --now container-berry.service
+systemctl enable --now container-controller.service
 popd
