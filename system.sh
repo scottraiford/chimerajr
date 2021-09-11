dfaf637f579760341ac48fc7e3084d4abe6175a2
scottraiford <88979496+scottraiford@users.noreply.github.com>
2021-09-11 14:56:16 -0400

    Moving container steps to containers.sh

--- system.sh
+++ system.sh
@@ -16,13 +16,9 @@ podman rm -a
 
 # Create container storage
 mkdir -p /srv/containers
-mkdir /srv/containers/portainer_data
 
 # Hash admin password
 hashed=$(podman run --rm httpd:2.4-alpine htpasswd -nbB admin $PORTAINER_ADMIN_PASSWORD | cut -d ":" -f 2)
-
-# Install Portainer
-podman run -d -p 9000:9000 --privileged --name=portainer --restart=always -v /run/podman/podman.sock:/var/run/docker.sock:Z -v /srv/containers/portainer_data:/data portainer/portainer-ce --admin-password=$hashed
 
 # Install host X server
 pacman -S --noconfirm xorg-server xf86-video-fbdev
