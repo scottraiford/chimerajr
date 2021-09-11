39cfee5c38a2af7f5e99852e310944bef13d33f1
scottraiford <88979496+scottraiford@users.noreply.github.com>
2021-09-11 16:09:33 -0400

    Properly name the controller and add descriptions

--- containers.sh
+++ containers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Build container images
+echo "Building images"
 podman build containers/berry/ -t berry
 podman build containers/controller/ -t controller
 
@@ -8,21 +9,27 @@ podman build containers/controller/ -t controller
 mkdir /srv/containers/portainer_data
 
 # Run Portainer
+echo "Running Portainer..."
 podman run -d -p 9000:9000 --privileged --name=portainer --restart=always -v /run/podman/podman.sock:/var/run/docker.sock:Z -v /srv/containers/portainer_data:/data portainer/portainer-ce --admin-password=$hashed
 
 # Run Berry
+echo "Running Berry..."
 podman run -d --name berry --restart always -v /home/chimerajr:/home/chimerajr -v /tmp/.X11-unix/:/tmp/.X11-unix/ -e DISPLAY berry
 
 # Run Controller
-podman run -d --name berry --restart always -v /home/chimerajr:/home/chimerajr -v /tmp/.X11-unix/:/tmp/.X11-unix/ -e DISPLAY controller
+echo "Running Controller..."
+podman run -d --name controller --restart always -v /home/chimerajr:/home/chimerajr -v /tmp/.X11-unix/:/tmp/.X11-unix/ -e DISPLAY controller
 
 # Create systemd unit files to rebuild containers on startup
+echo "Creating unit files..."
 pushd /usr/lib/systemd/system/
 podman generate systemd -n --new -f portainer
 podman generate systemd -n --new -f berry
 podman generate systemd -n --new -f controller
+popd
+
+echo "Reloading and enabling systemd...
 systemctl daemon-reload
 systemctl enable --now container-portainer.service
 systemctl enable --now container-berry.service
 systemctl enable --now container-controller.service
-popd
