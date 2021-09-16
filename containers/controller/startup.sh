f4d5accb0e25f7821086ed5fb5a59e94fb2f13fb
scottraiford <88979496+scottraiford@users.noreply.github.com>
2021-09-16 02:43:36 -0400

    Decouple dispatcher from container execution

--- containers/controller/startup.sh
+++ containers/controller/startup.sh
@@ -9,4 +9,6 @@ python3 gesture-sensor.py > /tmp/gestures &
 
 # Monitor the FIFO pipe
 # Commands are sent to berryc
-su chimerajr -c "sh dispatcher.sh"
+su chimerajr -c "sh dispatcher.sh" &
+
+while [ true ]; do; sleep 1; done
