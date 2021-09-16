585fd44f50e2058838e5e069ed6dfc95876aebec
scottraiford <88979496+scottraiford@users.noreply.github.com>
2021-09-16 02:41:01 -0400

    Add DPMS to dispatcher

--- containers/controller/dispatcher.sh
+++ containers/controller/dispatcher.sh
@@ -4,6 +4,7 @@ while true; do
   if read -r line ; then
     # This will be smarter in the future... but for now, this should work.
     echo "Line is $line"
+    xset dpms force on
     berryc cycle_focus
   fi
 done < /tmp/gestures
